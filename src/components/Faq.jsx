8d9347cc13b4d772b25a212cb8d6ce7a1c00273f
Jenin <jeninimmi2003@gmail.com>
2022-07-28 01:11:35 +0530

    ➕ Add `FAQ` structure

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,7 +1,61 @@
+import styles from "../styles/Faq.module.css";
+
 export default function FAQPage() {
   return (
-    <div>
-      <h2>This is FAQ Page</h2>
+    <div class={styles.FaqContainer}>
+      <h2>FAQ Section</h2>
+      <div>
+        <ul id={styles.accordion}>
+          <li>
+            <label htmlFor="first">
+              What is an Accordion? <span>&#x3e;</span>
+            </label>
+            <input type="radio" name="accordian" id="first" />
+            <div class="content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+                vel asperiores exercitationem, consequatur fuga modi pariatur.
+                Nihil illo minus perspiciatis eos! Tempora alias magnam enim
+                natus? Quis at corrupti doloribus porro, vel recusandae debitis
+                vitae dignissimos nostrum cupiditate assumenda et tempora odit
+                iste adipisci beatae maiores quod ipsa a odio.
+              </p>
+            </div>
+          </li>
+          <li>
+            <label htmlFor="second">
+              How to create Accordion <span>&#x3e;</span>
+            </label>
+            <input type="radio" name="accordian" id="second" />
+            <div class="content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+                vel asperiores exercitationem, consequatur fuga modi pariatur.
+                Nihil illo minus perspiciatis eos! Tempora alias magnam enim
+                natus? Quis at corrupti doloribus porro, vel recusandae debitis
+                vitae dignissimos nostrum cupiditate assumenda et tempora odit
+                iste adipisci beatae maiores quod ipsa a odio.
+              </p>
+            </div>
+          </li>
+          <li>
+            <label htmlFor="third">
+              Where to use it? <span>&#x3e;</span>
+            </label>
+            <input type="radio" name="accordian" id="third" />
+            <div class="content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
+                vel asperiores exercitationem, consequatur fuga modi pariatur.
+                Nihil illo minus perspiciatis eos! Tempora alias magnam enim
+                natus? Quis at corrupti doloribus porro, vel recusandae debitis
+                vitae dignissimos nostrum cupiditate assumenda et tempora odit
+                iste adipisci beatae maiores quod ipsa a odio.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
